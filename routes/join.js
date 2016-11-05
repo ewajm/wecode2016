b27c724142955fe0093038d3abac9de3b921da38
John SJ Anderson <john@genehack.org>
2016-11-05 11:04:22 -0700

    welcome page

--- routes/join.js
+++ routes/join.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/', function(req, res, next) {
-//   res.render('twitter');
-// });
+router.post('/', function(req, res, next) {
+   res.render('join');
+});
 
 module.exports = router;
