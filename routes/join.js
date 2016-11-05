6043114c1d29608fa25cb1075334b4a6129edf7c
Molly <molly@lumenlearning.com>
2016-11-05 11:39:13 -0700

    Fix pick route, minor form edits, grab steve

--- routes/join.js
+++ routes/join.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var app=express();
+var bodyParser= require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 router.post('/', function(req, res, next) {
-   res.render('join');
+   var name = req.body.formWelcomeName;
+   res.render('join', {
+     name: name
+   });
 });
 
 module.exports = router;
