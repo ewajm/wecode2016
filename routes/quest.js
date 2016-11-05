2cab09016376fac7aea88ea2b1c67ce3d17bb356
John SJ Anderson <john@genehack.org>
2016-11-05 13:49:01 -0700

    Set up individual quest page

--- routes/quest.js
+++ routes/quest.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('quest', { layout: 'sidebar_layout' });
+router.get('/:quest', function(req, res, next) {
+  function rightQuest (elem) { return elem.hashtag === req.params.quest; }
+
+  var sesh = req.session;
+
+  var quest = req.session.quests.find(rightQuest);
+
+  res.render('quest', { layout: 'sidebar_layout' , quest: quest});
 });
 
-router.post('/', function(req, res, next) {
+router.post('/:quest', function(req, res, next) {
+  function rightQuest (elem) { return elem.hashtag === req.params.quest; }
 
-  res.render('friends');
+  var sesh = req.session;
+
+  var quest = req.session.quests.find(rightQuest);
+
+  // FIXME|TODO look at the stuff that was posted and subtract the HP
+  // and detect whether or not the monster has been killed
+  var activity = get_the_activity; // fixme
+  var duration = get_the_duration; // fixme
+  var damage   = activity * duration; // fixme
+  quest.hp -= damage;
+
+  var template = 'quest'; // reload the page by default
+  if( quest.hp <= 0 ) { template = 'win'; } //
+  res.render(template, { layout: 'sidebar_layout' , quest: quest, post: "posted"});
 });
 
 module.exports = router;
