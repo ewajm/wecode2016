aae42b2066953b374c4efb4527fec14940ce2e5b
John SJ Anderson <john@genehack.org>
2016-11-05 10:34:36 -0700

    friends page

--- routes/friends.js
+++ routes/friends.js
@@ -1,8 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/', function(req, res, next) {
-//   res.render('twitter');
-// });
+router.get('/', function(req, res, next) {
+  res.render('friends', {
+    friends: [
+      {
+        id: 1,
+        value: "dr_strange",
+        name: "Doctor Strange"
+      },
+      {
+        id: 2,
+        value: "dr_who",
+        name: "Doctor Who"
+      },
+      {
+        id: 3,
+        value: "dr_welby",
+        name: "Doctor Marcus Welby MD"
+      },
+      {
+        id: 4,
+        value: "dr_demento",
+        name: "Doctor Demento"
+      },
+      {
+        id: 5,
+        value: "dr_suess",
+        name: "Doctor Suess"
+      },
+      {
+        id: 6,
+        value: "dr_zhivago",
+        name: "Doctor Zhivago"
+      },
+      {
+        id: 7,
+        value: "dr_frankenstein",
+        name: "Doctor Frankenstein"
+      },
+      {
+        id: 8,
+        value: "dr_curie",
+        name: "Doctor Curie"
+      },
+      {
+        id: 9,
+        value: "dr_jekyll",
+        name: "Doctor Jekyll"
+      }
+    ]
+  });
+});
 
 module.exports = router;
