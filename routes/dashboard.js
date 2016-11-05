213453c998cb70846be9721e2aae344ca7d1f363
John SJ Anderson <john@genehack.org>
2016-11-05 15:04:15 -0700

    misc fixes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,10 +6,20 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+router.get('/', function(req, res, next){
+  var sesh = req.session;
+  res.render('dashboard', {
+    layout: 'sidebar_layout' ,
+    user: sesh.name,
+    active: sesh.quests.filter(function (elem) { return elem.state === 'active'; }),
+    pending: sesh.quests.filter(function (elem) { return elem.state === 'pending'; }),
+    ignored: sesh.quests.filter(function (elem) { return elem.state === 'ignored'; }),
+    completed: sesh.quests.filter(function (elem) { return elem.state === 'completed'; })
+  });
+});
+
 router.post('/', function(req, res, next) {
   var sesh = req.session;
-  var quest = sesh.quests[sesh.quests.length - 1];
-  quest.friends = req.body.friends;
   res.render('dashboard', {
     layout: 'sidebar_layout' ,
     user: sesh.name,
