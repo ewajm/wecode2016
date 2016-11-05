65d62be0fd926d4220fe104eb151e8f8bc8075a7
John SJ Anderson <john@genehack.org>
2016-11-05 12:06:26 -0700

    add session support

--- routes/twitter.js
+++ routes/twitter.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('twitter');
+  var sesh = req.session;
+  res.render('twitter', { name: sesh.name });
 });
 
 router.post('/fake-oauth', function(req, res, next) {
