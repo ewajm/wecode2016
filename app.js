94a90382c70accfe2aaa751a5a1430dd92119a4d
Lacayo, Claudio <claudio.lacayo@nike.com>
2016-11-05 10:09:40 -0700

    Added routes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('test',test);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +23,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', index);
 app.use('/users', users);
+app.use('/join', join);
+app.use('/twitter', twitter);
+app.use('/pick', pick);
+app.use('/friends', friends);
+app.use('/dashboard', dashboard);
+app.use('/quest', quest);
+app.use('/add', add);
+app.use('/end', end);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
