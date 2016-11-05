eac4a4acf88a614a99f3dcfd9219105763e7c2f3
John SJ Anderson <john@genehack.org>
2016-11-05 13:28:13 -0700

    store friends in quest; silly quest display on dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express    = require('express');
+var router     = express.Router();
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 router.post('/', function(req, res, next) {
-  res.render('dashboard', { layout: 'sidebar_layout' });
+  var sesh = req.session;
+  var quest = sesh.quests[sesh.quests.length - 1];
+  quest.friends = req.body.friends;
+
+  res.render('dashboard', { layout: 'sidebar_layout' , quest: quest });
 });
 
 module.exports = router;
