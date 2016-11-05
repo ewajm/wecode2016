6f525b3dae73c64186478dcf7fffbc6992ec8cf1
John SJ Anderson <john@genehack.org>
2016-11-05 11:00:07 -0700

    add comment

--- routes/friends.js
+++ routes/friends.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+  // have code here to fetch friends list from twitter
   res.render('friends', {
     friends: [
       {
