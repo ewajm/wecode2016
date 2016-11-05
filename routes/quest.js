d2a9f9e6fa86b4c3933f5627c84670576bc36500
Molly <molly@lumenlearning.com>
2016-11-05 11:58:55 -0700

    start fake quest json

--- routes/quest.js
+++ routes/quest.js
@@ -6,8 +6,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  
+
   res.render('friends');
 });
 
 module.exports = router;
+
+quests: [
+  {
+    id: 1,
+    value: "dr_strange",
+    name: "Doctor Strange"
+  },
+  {
+    id: 2,
+    value: "dr_who",
+    name: "Doctor Who"
+  },
